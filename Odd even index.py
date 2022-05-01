5f1d8e26b928c05566a60de8324b393773eac902
BHUKYA VEERANNA <104159515+Veeranna07@users.noreply.github.com>
2022-05-01 17:01:03 +0530

    Create Odd even index.py

---
+++ Odd even index.py
@@ -0,0 +1,27 @@
+'''
+
+                       Odd even index
+                       Given a string, print all the letters present at odd index, followed by the letters present at even index.
+
+Input Format
+Input contains a string S, consisting of ascii characters.
+
+Constraints
+1 <= len(S) <= 100
+
+Output Format
+Print letters present at odd index, followed by the letters present at even index.
+
+Sample Input 0
+afdg5tg
+
+Sample Output 0
+fgtad5g
+
+Explanation 0
+Self Explanatory
+
+'''
+
+s=str(input())
+print(s[1::2],s[::2], sep='')
