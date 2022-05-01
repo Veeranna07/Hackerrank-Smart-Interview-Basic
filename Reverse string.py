a2beea05090b0ed054084aaf5acc866ca120044f
BHUKYA VEERANNA <104159515+Veeranna07@users.noreply.github.com>
2022-05-01 14:46:13 +0530

    Create Reverse string.py

---
+++ Reverse string.py
@@ -0,0 +1,28 @@
+'''
+
+               Reverse string
+               Given a string, reverse the given string in-place and then print it.
+               Note: Do not use any inbuilt functions/libraries for your main logic.
+
+Input Format
+Input contains a string S, consisting of ascii characters.
+
+Constraints
+1 <= len(s) <= 100
+
+Output Format
+Print the reversed string.
+
+Sample Input 0
+fdsrd
+
+Sample Output 0
+drsdf
+
+Explanation 0
+Self Explanatory
+
+'''
+
+s=str(input())
+print(s[::-1])
