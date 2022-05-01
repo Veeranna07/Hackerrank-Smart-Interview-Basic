ac860324521396476b871afad5c2c666877729ff
BHUKYA VEERANNA <104159515+Veeranna07@users.noreply.github.com>
2022-05-01 11:54:32 +0530

    Create Digits sum.py

---
+++ Digits sum.py
@@ -0,0 +1,32 @@
+'''
+
+                    Digits sum
+                    Given a non-negative integer - N, print the sum of digits of the given number.
+
+Input Format
+First and only line of input contains a non-negative integer N.
+
+Constraints
+0 <= length(N) <= 103
+
+Output Format
+Print the sum of digits of the given number.
+
+Sample Input 0
+164
+
+Sample Output 0
+11
+
+Explanation 0
+Self Explanatory
+
+'''
+number=int(input())
+addition=0
+modulo=0
+while number!=0:
+    modulo=number%10
+    addition+=modulo
+    number//=10
+print(addition)
