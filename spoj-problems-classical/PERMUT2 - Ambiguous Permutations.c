4f642c663c72427437a500a2d3b72e2ad05954e7
BHUKYA VEERANNA <104159515+Veeranna07@users.noreply.github.com>
2022-05-15 17:36:03 +0530

    Create PERMUT2 - Ambiguous Permutations.c

---
+++ spoj-problems-classical/PERMUT2 - Ambiguous Permutations.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100001],i;
+    while(1)
+    {
+        scanf("%d",&n);
+        if(n==0)break;
+        for(i=1;i<=n;i++)
+             scanf("%d",&a[i]);
+        for(i=1;i<=n;i++)
+        {
+            if(a[a[i]]!=i){printf("not ambiguous \n");break;}
+            if(i==n)printf("ambiguous \n");
+        }
+    }
+}
